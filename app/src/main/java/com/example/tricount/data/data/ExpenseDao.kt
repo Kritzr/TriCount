1c3f57bb89c0114188976c1379ff241efdff81cb
KrithikaRavishankar <kritstudy15@gail.com>
2026-02-03 16:34:56 +0530

    FEAT: trying new db using rooms (orms)

--- app/src/main/java/com/example/tricount/data/data/ExpenseDao.kt
+++ app/src/main/java/com/example/tricount/data/data/ExpenseDao.kt
@@ -1,18 +1,11 @@
-package com.example.tricount.data.data
+package com.example.tricount.data
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.Query
 
 @Dao
-interface ExpenseDao {
+interface TricountDao {
 
     @Insert
-    suspend fun insertExpense(expense: ExpenseEntity)
-
-    @Query("SELECT * FROM expenses WHERE tricountId = :tricountId")
-    suspend fun getExpensesForTricount(tricountId: Int): List<ExpenseEntity>
-
-    @Query("DELETE FROM expenses")
-    suspend fun clearAll()
+    suspend fun insertTricount(tricount: TricountEntity)
 }
---
+++ app/src/main/java/com/example/tricount/data/data/TriCountEntity.kt
@@ -0,0 +1,12 @@
+package com.example.tricount.data.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tricounts")
+data class TricountEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val description: String
+)
